d0702d5865ba053791ac1f8de73c735e26eec714
iwxi <282113191@qq.com>
2019-01-23 23:15:19 +0800

    index-swiper

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import '__styles__/reset.css'
 import '__styles__/border.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
+Vue.use(VueAwesomeSwiper, /* { default global options } */);
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="wrapper">
+  	<swiper :options="swiperOption">
+	    <!-- slides -->
+	    <swiper-slide v-for="(item) of swiperList" :key="item.id">
+	    	<img class="swiper-image" :src="item.url" />
+	    </swiper-slide>
+	    <!-- Optional controls -->
+	    <div class="swiper-pagination"  slot="pagination"></div>
+  	</swiper>
+  	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		name: "HomeSwiper",
+		data () {
+			return {
+				swiperOption: {
+					pagination: '.swiper-pagination',
+					loop: true
+				},
+				swiperList: [{
+					id: 'img1',
+					url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548265021620&di=f038485903cc0219d9b8a71c4c6f4f9f&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01398c5942618ca8012193a3f97976.jpg'
+				},
+				{
+					id: 'img2',
+					url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548264972406&di=606bdbb62202abcc089d6eba29f96201&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F019a0558be22d6a801219c77d0578a.jpg%402o.jpg'
+				}]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	// 宽高比固定不能使用height去设置高度，height的高度比例是对应的父级元素
+	// 需要使用 height:0 overfloat:hidden padding-bottom:比例 来设置
+	.wrapper
+		width: 100%
+		height: 0
+		overfloat: hidden
+		padding-bottom: 39.06%;
+		.swiper-image
+			width: 100%
+	// >>> 穿透
+	.wrapper >>> .swiper-pagination-bullet-active
+		background: #FFF !important
+</style>
