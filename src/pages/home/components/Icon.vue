76cc4625dde0f148c24a5cb90c51ff349a377005
iwxi <282113191@qq.com>
2019-04-16 21:42:12 +0800

    index-finish

--- src/pages/home/components/Icon.vue
+++ src/pages/home/components/Icon.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="icons">
-	  	<swiper>
+	  	<swiper :options="swiperOption">
 	  		<swiper-slide v-for="(page, index) of pages" :key="index">
 				<div class="icon" v-for="vo of page" :key="vo.id">
 					<div class="icon-img">
@@ -16,51 +16,20 @@
 <script type="text/javascript">
 	export default {
 		name: 'HomeIcon',
+		props: {
+			list: Array
+		},
 		data: function () {
 			return {
-				iconList: [{
-					id: '000001',
-					imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
-					desc: '景点门票'
-				}, {
-					id: '000002',
-					imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
-					desc: '海洋馆'
-				}, {
-					id: '000003',
-					imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
-					desc: '游乐场'
-				}, {
-					id: '000004',
-					imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
-					desc: '泡温泉'
-				}, {
-					id: '000005',
-					imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
-					desc: '一日游'
-				}, {
-					id: '000006',
-					imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20191/de711920c71e38287d9f95f7910aa1aa.png',
-					desc: '精品小团'
-				}, {
-					id: '000007',
-					imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
-					desc: '三坊七巷'
-				}, {
-					id: '000008',
-					imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/87/20656da0ac042002.png',
-					desc: '武夷山'
-				}, {
-					id: '000009',
-					imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
-					desc: '六神花露水'
-				}]
+				swiperOption: {
+					autoplay: false
+				}
 			}
 		},
 		computed: {
 			pages: function () {
 				const pages = []
-				this.iconList.forEach((val, key) => {
+				this.list.forEach((val, key) => {
 					const page = [Math.floor(key / 8)]
 					if(!pages[page]) {
 						pages[page] = []
--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -1,18 +1,47 @@
 <template>
 	<div>
-		fd
+		<div class="title">{{title}}</div>
+		<ul>
+			<li class="item border-bottom" v-for="(item) of list" :key="item.id">
+				<div class="item-img-wrapper">
+					<img :src="item.imgUrl" class="item-img" />
+				</div>
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+				</div>
+			</li>
+		</ul>
 	</div>
 </template>
 
 <script type="text/javascript">
 	export default {
 		name: 'HomeWeekend',
+		props: {
+			list: Array
+		},
 		data: function () {
-			return {}
+			return {
+				title: "周末去哪儿"
+			}
 		}
 	}
 </script>
 
 <style lang="stylus" scoped>
-				
+	.title
+		line-height: .8rem
+		background-color: #EEE
+		text-indent: .2rem
+	.item-img-wrapper
+		overflow: hidden
+		height: 0
+		padding-bottom: 37.09%
+		.item-img
+			width: 100%
+	.item-info
+		padding: .1rem
+		.item-title
+			line-height: .54rem
 </style>
