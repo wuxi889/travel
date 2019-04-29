334a95f0b1c9613668242b7d260c5b58bd778bfb
iwxi <282113191@qq.com>
2019-04-29 20:44:40 +0800

    search-finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,16 +1,62 @@
 <template>
-	<div class="search">
-		<input class="search-input" type="text" placeholder="输入城市名或拼音">
+	<div>
+		<div class="search">
+			<input class="search-input" type="text" placeholder="输入城市名或拼音" v-model="keyword">
+		</div>
+		<div class="search-content" ref="search" v-show="keyword">
+			<ul>
+				<li class="search-item border-bottom" v-for="(item, index) of list" :key="index">{{item.name}}</li>
+				<li class="search-item border-bottom" v-show="hasNoData">没有找到匹配地址</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
 <script type="text/javascript">
+	import Bscroll from 'better-scroll'
 	export default {
 		name: 'CitySearch',
+		props: {
+			cities: Object
+		},
 		data () {
 			return {
-				
+				keyword: '',
+				list: [],
+				timer: null
 			}
+		},
+		computed: {
+			hasNoData () {
+				return !this.list.length
+			}
+		},
+		watch: {
+			keyword () {
+				if (this.timer) {
+					clearTimeout(this.timer)
+				}
+				if (!this.keyword) {
+					this.list = []
+					return 
+				}
+				this.timer = setTimeout(() => {
+					const result = []
+					for (let i in this.cities) {
+						this.cities[i].forEach((value) => {
+							if (value.spell.indexOf(this.keyword) > -1 ||
+								value.name.indexOf(this.keyword) > -1) {
+								result.push(value)
+							}
+						})
+					}
+					this.list = result
+				}, 100)
+			}
+		},
+		mounted () {
+			this.scroll = new Bscroll(this.$refs['search'])
+
 		}
 	}
 </script>
@@ -30,4 +76,18 @@
 			border-radius: .06rem
 			color: #666
 			padding: 0 .1rem
+	.search-content
+		position: absolute
+		overflow: hidden
+		top: 1.58rem
+		left: 0
+		bottom: 0
+		right: 0
+		background-color: #EEE
+		z-index: 1
+		.search-item
+			line-height: .62rem
+			padding-left: .2rem
+			color: #666
+			background-color: #FFF
 </style>
